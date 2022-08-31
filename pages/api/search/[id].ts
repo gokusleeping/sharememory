1bc0f1d265e7a18d14bfa42daa6dd6db6ee5ad75
Ranvir Singh <gokusleeping@gmail.com>
2022-09-01 03:41:36 +0530

    fixed vercel deployment issues

--- pages/api/search/[id].ts
+++ pages/api/search/[id].ts
@@ -4,6 +4,7 @@ import { searchPostsQuery } from '../../../utils/queries';
 import { client } from '../../../utils/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+ 
   if (req.method === 'GET') {
     const { id } = req.query;
 
