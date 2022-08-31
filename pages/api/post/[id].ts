c2f0461362f7472d571be83453900a895792b604
Ranvir Singh <gokusleeping@gmail.com>
2022-09-01 04:53:38 +0530

    added cors origin at backend

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -6,6 +6,7 @@ import { uuid } from 'uuidv4';
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
+    
     const { id } = req.query;
     const query = postDetailQuery(id!);
 
