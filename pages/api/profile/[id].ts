831a70d65d554fe0985f2f49f964895f92f36b2d
Ranvir Singh <gokusleeping@gmail.com>
2022-09-01 03:43:30 +0530

    fixed vercel deployment issues

--- pages/api/profile/[id].ts
+++ pages/api/profile/[id].ts
@@ -17,6 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const data = { user: user[0], userVideos, userLikedVideos };
 
+    
     res.status(200).json(data);
   }
 }
